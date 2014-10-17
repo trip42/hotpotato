31296b24043b9aba1e8fe0891b2ca7631c8e591e
Brian Brinegar <bbrinegar@exacttarget.com>
2014-10-17 17:27:45 -0400

    First commit

--- server.js
+++ server.js
@@ -1,136 +1,102 @@
 var
-    app = require('http').createServer(handler)
-    , io = require('socket.io').listen(app)
-    , fs = require('fs')
-    , bombers = []
+    express = require( 'express' )
+    , app = express()
+    , server = require( 'http' ).Server( app )
+    , io = require( 'socket.io' ).listen( server )
     , port = process.env.PORT || 5000
+    , players = []
+    , maxPlayers = 3
+    , potatoPosition = 0
+    , timer = null
+    , host = null
 ;
 
-app.listen(port);
+server.listen( port );
 
-function handler(req, res) {
-    fs.readFile(
-        __dirname + '/index.html', 
-        function(err, data) {
-            res.writeHead(200);
-            res.end(data);
-        }
-    );
-}
-
-function randInt(min, max) {
-    return parseInt( Math.random()*max + min );
-}
-
-function Bomber( name ) {
-    this.id = 'bomber-' + bombers.length;
-    this.name = name;
-    this.color = 'rgb(' + [ randInt(64, 255), randInt(64, 255), randInt(64, 255) ].join(',') + ')';
-    this.x = 0;
-    this.y = 0;
-    this.exploding = false;
-    this.score = 0;
-
-    bombers.push(this);
-}
-
-Bomber.prototype.addPoints = function(points) {
-    this.score += points;
-};
-
-Bomber.prototype.move = function(x,y) {
-    if ( !this.exploding ) {
-        this.x = x;
-        this.y = y;
-        io.sockets.emit('move', this);
-    }
-};
-
-Bomber.prototype.ignite = function() {
-    if ( !this.exploding ) {
-        this.exploding = true;
-
-        io.sockets.emit('ignite', this);
-        setTimeout( this.explode.bind(this), 500 );
-    }
-};
-
-Bomber.prototype.explode = function() {
-    // take out any bombs within 100 pixels of the bomber
-    var i, b;
-  
-    for ( i in bombers ) {
-        b = bombers[i];
-        if ( b !== this && !b.exploding && Math.sqrt( Math.pow(b.x - this.x, 2) + Math.pow(b.y - this.y, 2) ) <= 100 ) {
-            b.ignite();
-            this.addPoints(50);
-        }
-    }
-
-    io.sockets.emit('explode', this);
-    setTimeout( this.reset.bind(this) , 1000 );
-};
-
-Bomber.prototype.reset = function() {
-    this.exploding = false;
-    io.sockets.emit('reset', this);
-}
-
-// socket stuff
-
-io.sockets.on('connection', function(socket) {
-    socket.on('join', function(name) {
-        var bomber, i;
-    
-        // tell the new client about everyone else
-    
-        for (i in bombers) {
-            socket.emit('join', bombers[i]);
-        }
-    
-        // create a new bomber for this player
-    
-        bomber = new Bomber(name);
-    
-        // tell everyone else that we joined
-    
-        io.sockets.emit('join', bomber );
-    
-        // handle our move, ignite, and disconnect actions
-    
-        socket.on('move', function( x, y ) {
-            bomber.move( x, y );
-        });
-    
-        socket.on('ignite', function() {
-            bomber.ignite();
-        });
-    
-        socket.on('disconnect', function() {
-            delete bombers[bombers.indexOf(bomber)];
-            io.sockets.emit('disconnect', bomber );
-        });
-    });
+app.get( '/', function( req, res ) {
+    res.sendFile( __dirname + '/static/client.html' );
 });
 
-setInterval( function() {
-    var 
-        i,
-        scores = []
-    ;
+// serve static files
+app.use( express.static( __dirname + '/static/' ) );
 
-    for (i=0; i<bombers.length; i++) {
-        if ( !!bombers[i] ) {
-            scores.push({
-                color: bombers[i].color,
-                name: bombers[i].name,
-                score: bombers[i].score
-            });
-        }
+function movePotato( offset ) {
+    // move the potato
+    potatoPosition += offset;
 
-        scores.sort(function(a,b) { return b.score - a.score; });
+    if ( potatoPosition >= players.length ) {
+        potatoPosition = 0;
+    } else if ( potatoPosition < 0 ) {
+        potatoPosition = players.length - 1;
     }
 
-    io.sockets.emit( 'score', scores );
-}, 2000);
+    if ( players[ potatoPosition ].alive ) {
+        console.log( players[ potatoPosition ].name );
+        io.sockets.emit( 'potato moved', potatoPosition );
+    } else {
+        movePotato( offset );
+    }
+}
 
+function startTimer() {
+    var time = Math.floor( Math.random() * ( 30000 - 5000 ) ) + 5000;
+
+    io.sockets.emit( 'start' );
+    console.log( 'start', time );
+
+    setTimeout( function() {
+        var player = players[ potatoPosition ];
+
+        player.alive = false;
+
+        console.log( player.name, ' is out' );
+        movePotato( 1 );
+
+        io.sockets.emit( 'stop', player.clientId );
+
+        setTimeout( startTimer, 10000 );
+
+    }, time )
+}
+
+io.on( 'connection', function( socket ) {
+    socket.on( 'join', function( name ) {
+        if ( players.length > maxPlayers ) {
+            socket.emit( 'joined', 'full' );
+            return;
+        } else {
+            var player = {
+                name: name,
+                alive: true,
+                clientId: players.length
+            };
+
+            players.push( player );
+
+            socket.on( 'pass', function() {
+                if ( player.clientId === potatoPosition ) {
+                    movePotato( 1 );
+                }
+            } );
+
+            socket.on( 'disconnect', function() {
+                player.alive = false;
+                if ( player.clientId === potatoPosition ) {
+                    movePotato( 1 );
+                }
+            } );
+
+            console.log( 'Player', player.name, 'joined' );
+            socket.emit( 'joined', 'ok', player.clientId );
+            socket.emit( 'potato moved', potatoPosition );
+
+            if ( players.length === maxPlayers ) {
+                startTimer();
+            } else {
+                console.log( 'need', maxPlayers - players.length, 'more players' );
+            }
+        }
+    } );
+} );
+
+console.log( 'server started' );
